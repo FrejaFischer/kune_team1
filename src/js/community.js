bc84022dba6643a688c6ab8cb3aa16f642bb841b
Freja Fischer <frej657e@stud.kea.dk>
2023-03-23 10:56:43 +0100

    maybe last commit

---
+++ src/js/community.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", startCommunity);
+
+function startCommunity() {
+  document.querySelector("#about_button").classList.add("button_active");
+
+  document.querySelector("#about_button").addEventListener("click", showAbout);
+  document.querySelector("#volunteer_button").addEventListener("click", showVolunteer);
+}
+
+function showAbout() {
+  document.querySelector("#volunteer").classList.add("hide");
+  document.querySelector("#about").classList.remove("hide");
+  document.querySelector("#volunteer_button").classList.remove("button_active");
+
+  document.querySelector("#volunteer_button").addEventListener("click", showVolunteer);
+}
+
+function showVolunteer() {
+  document.querySelector("#volunteer_button").classList.add("button_active");
+  document.querySelector("#about_button").classList.remove("button_active");
+
+  document.querySelector("#volunteer").classList.remove("hide");
+  document.querySelector("#about").classList.add("hide");
+
+  document.querySelector("#about_button").addEventListener("click", showAbout);
+}
